db1798e165793e038215338c7cd8706c0e59711c
Matthew Haid <wsd6vn@virginia.edu>
2023-10-21 15:08:11 -0400

    changed divs to be react based

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,27 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
-import * as ml5 from "ml5";
-import Webcam from "react-webcam"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="App">
-          <Webcam />
-        </div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button type="button" onclick="init()">
+        Start
+      </button>
+      <div id="webcam-container"></div>
+      <div id="label-container"></div>
     </div>
   );
 }
