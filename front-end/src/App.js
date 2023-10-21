f0a60ad31c109121b4b611a3397eb898394db953
SurvivrrHayde <wsd6vn@virginia.edu>
2023-10-21 15:49:51 -0400

    fixed rendering

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,8 +3,13 @@ import { useEffect } from 'react';
 
 function App() {
 
+
+
   return (
     <div className="App">
+      <button onClick={window.init}>
+        start
+      </button>
       <div id="webcam-container"></div>
       <div id="label-container"></div>
     </div>
